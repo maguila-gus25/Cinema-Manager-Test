6fd566d6eb22f2cc061e272d088dd9144a5ccddc
Gustavo <166073286+maguila-gus25@users.noreply.github.com>
2024-09-16 19:49:02 -0300

    Create filme.py

---
+++ filme.py
@@ -0,0 +1,8 @@
+#Classes
+#Filme
+#Atributos: título, duração, gênero, classificação etária, sinopse.
+#Métodos: adicionarFilme(), atualizarFilme(), removerFilme().
+
+class Filme:
+  def __init__(self):
+    
