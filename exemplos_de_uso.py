27ecf1187791e81f99b7a304d54f7cbc3c3feaea
Gustavo <166073286+maguila-gus25@users.noreply.github.com>
2024-09-18 23:54:48 -0300

    Update exemplos_de_uso.py

--- exemplos_de_uso.py
+++ exemplos_de_uso.py
@@ -34,3 +34,41 @@ print(resultado)
 
 resultado_remover_cliente = cliente1.removerCliente()
 print(resultado_remover_cliente)
+
+================================================================
+
+
+sala1 = Sala(1, 100, "3D")
+print(20*"=")
+print(sala1.adicionarSala())
+
+filme1 = Filme("Inception", "148 min", "Sci-Fi", 14, "Um ladrão especializado em extrair segredos do subconsciente das pessoas é oferecido a chance de ter sua vida de volta, se ele conseguir realizar uma tarefa impossível: plantar uma ideia na mente de um CEO.")
+print(20*"=")
+print(filme1.adicionarFilme())
+
+sessao1 = Sessao(filme1, sala1, "11/09/2024", "17h30", "43")
+print(20*"=")
+print(sessao1.adicionarSessao())
+print(20*"=")
+print(sala1)
+print(20*"=")
+print(sessao1.removerSessao())
+
+cliente1 = Cliente("Gustavo Ramos", "(48)98456-6284", "gustavohcramos@gmail.com", "")
+print(cliente1)
+
+print(cliente1.cadastrarCliente())
+print(cliente1.atualizarCliente("Felipe Ramos", "(48)98484-3030", "felipebcramos@gmail.com", ""))
+
+print(cliente1.removerCliente())
+
+
+ingresso = Ingresso(sessao1, assento="A1", preco=30.0)
+
+
+print(ingresso.verificarDisponibilidade())  # True
+ingresso.emitirIngresso()  # Vende o ingresso
+print(ingresso.verificarDisponibilidade())  # False
+ingresso.cancelarIngresso()  # Cancela o ingresso
+
+
