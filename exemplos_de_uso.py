b5fe547ac4c5cf166d635dce1e4229b93a25fbe5
Gustavo <166073286+maguila-gus25@users.noreply.github.com>
2024-09-20 18:51:54 -0300

    Update exemplos_de_uso.py

--- exemplos_de_uso.py
+++ exemplos_de_uso.py
@@ -1,9 +1,33 @@
 
 #Exemplo de Uso 1: Adicionar um Filme
 
+# Criação de vários filmes usando a classe Filme
+
 filme1 = Filme(titulo="O Poderoso Chefão", duracao="2h55min", genero="Drama", classificacao_etaria=18, sinopse="A saga da família Corleone.")
-resultado = filme1.adicionarFilme()
-print(resultado)
+filme2 = Filme(titulo="Vingadores: Ultimato", duracao="3h", genero="Ação", classificacao_etaria=12, sinopse="Os heróis da Marvel enfrentam Thanos em uma batalha épica.")
+filme3 = Filme(titulo="Matrix", duracao="2h16min", genero="Ficção Científica", classificacao_etaria=14, sinopse="Um hacker descobre a verdade sobre sua realidade e o papel que ele desempenha.")
+filme4 = Filme(titulo="A Origem", duracao="2h28min", genero="Suspense", classificacao_etaria=14, sinopse="Um especialista em invadir sonhos embarca em uma missão perigosa.")
+filme5 = Filme(titulo="Coringa", duracao="2h02min", genero="Drama", classificacao_etaria=16, sinopse="A história da transformação de Arthur Fleck no icônico vilão Coringa.")
+filme6 = Filme(titulo="Toy Story 4", duracao="1h40min", genero="Animação", classificacao_etaria=L, sinopse="Woody e seus amigos embarcam em mais uma aventura, agora com o novo brinquedo Forky.")
+filme7 = Filme(titulo="Parasita", duracao="2h12min", genero="Suspense", classificacao_etaria=16, sinopse="A história de duas famílias, uma rica e uma pobre, que se entrelaçam de maneira perigosa.")
+filme8 = Filme(titulo="Star Wars: O Despertar da Força", duracao="2h18min", genero="Ficção Científica", classificacao_etaria=12, sinopse="Uma nova geração de heróis luta contra a Primeira Ordem.")
+filme9 = Filme(titulo="Titanic", duracao="3h15min", genero="Romance", classificacao_etaria=12, sinopse="Uma história de amor durante a fatídica viagem do Titanic.")
+filme10 = Filme(titulo="Jurassic Park", duracao="2h7min", genero="Aventura", classificacao_etaria=10, sinopse="Cientistas recriam dinossauros, mas o parque não sai como o esperado.")
+
+filme1.adicionarFilme()
+filme2.adicionarFilme()
+filme3.adicionarFilme()
+filme4.adicionarFilme()
+filme5.adicionarFilme()
+filme6.adicionarFilme()
+filme7.adicionarFilme()
+filme8.adicionarFilme()
+filme9.adicionarFilme()
+filme10.adicionarFilme()
+
+#Criando Sessoes
+
+sessao2 = Sessao(filme=filme9, sala=Sala(2, 50, "VIP"), data="20/09/2024", horario="19:00", ingressos_disponiveis=50)
 
 #Exemplo de Uso 2: Atualizar um Filme
 
